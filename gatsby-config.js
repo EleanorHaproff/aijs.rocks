e733de3e858704d467787f29ad8eff0f61e7751f
Osama Jandali <osama.jandali@robovics.com>
2018-07-06 20:03:04 +0300

    Fix undisplayed blog images

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,23 @@ module.exports = {
         name: 'pages',
       },
     },
-    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            // It's important to specify the maxWidth (in pixels) of
+            // the content container as this plugin uses this as the
+            // base for generating different widths of each image.
+            maxWidth: 590,
+          },
+        },
+      ],
+    },
+  },
   ],
 
 
