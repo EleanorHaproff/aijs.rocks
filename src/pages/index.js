fe231985dfd49ac9a25e0dd5d5e3e5cbde1ff2d9
jawache <jawache@gmail..com>
2018-08-05 22:49:19 +0100

    Aligned images to center by default

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import styled from 'styled-components';
-import EllipsisText  from 'react-ellipsis-text';
+import styled from 'styled-components'
+import EllipsisText from 'react-ellipsis-text'
 
 import { Tag } from '../globalStyles'
 
@@ -15,8 +15,8 @@ const Post = styled.div`
   background: #232537;
   width: calc(50% - 16px);
   height: 440px;
-  border-radius:16px;
-  margin-bottom:32px;
+  border-radius: 16px;
+  margin-bottom: 32px;
   @media (max-width: 1024px) {
     width: 100%;
   }
@@ -30,21 +30,22 @@ const Thumbnail = styled.div`
   width: 100%;
   height: 200px;
   background-size: cover;
+  background-position: center;
 `
 const PostFooter = styled.div`
   padding: 16px;
 `
 const Description = styled.div`
   height: 57px;
-  width:100%;
+  width: 100%;
   overflow: hidden;
   text-overflow: ellipsis;
-  p{
-    color:white;
+  p {
+    color: white;
     font-size: 12px;
     margin: 0;
-    span{
-      color:white;
+    span {
+      color: white;
     }
   }
 `
@@ -56,10 +57,10 @@ const TagsWrapper = styled.ul`
   margin: 8px 0 0 0;
 `
 const Details = styled.div`
-  display:flex;
-  align-items:center;
-  justify-content:space-between;
-  color:white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
 `
 const AuthorWrapper = styled.div`
   display: flex;
@@ -77,7 +78,7 @@ const Date = styled.h6`
   color: #49e1c2;
 `
 export default function Index({ data }) {
-  const { edges: posts } = data.allMarkdownRemark;
+  const { edges: posts } = data.allMarkdownRemark
   console.log(posts)
   return (
     <PostsWrapper>
@@ -87,17 +88,32 @@ export default function Index({ data }) {
           return (
             <Post key={post.id}>
               <Link to={post.frontmatter.path}>
-                <PostTitle>
-                  {post.frontmatter.title}
-                </PostTitle>
-                <Thumbnail style={{backgroundImage:`url(${post.frontmatter.thumbnail.childImageSharp.responsiveSizes.src})`}} />
+                <PostTitle>{post.frontmatter.title}</PostTitle>
+                <Thumbnail
+                  style={{
+                    backgroundImage: `url(${
+                      post.frontmatter.thumbnail.childImageSharp.responsiveSizes
+                        .src
+                    })`,
+                  }}
+                />
               </Link>
               <PostFooter>
-                <Description><p> <EllipsisText text={post.frontmatter.shortDescription} length={150} /></p></Description>
+                <Description>
+                  <p>
+                    {' '}
+                    <EllipsisText
+                      text={post.frontmatter.shortDescription}
+                      length={150}
+                    />
+                  </p>
+                </Description>
                 <TagsWrapper>
-                  {post.frontmatter.tags.map(tag =>
-                    <Tag key={tag} style={{marginRight:8}}>{tag}</Tag>
-                  )}
+                  {post.frontmatter.tags.map(tag => (
+                    <Tag key={tag} style={{ marginRight: 8 }}>
+                      {tag}
+                    </Tag>
+                  ))}
                 </TagsWrapper>
                 <Details>
                   <AuthorWrapper>
@@ -107,7 +123,10 @@ export default function Index({ data }) {
                     />
                     <h6>{post.frontmatter.author}</h6>
                   </AuthorWrapper>
-                  <Date><i className="icon-clock"/>{post.frontmatter.date}</Date>
+                  <Date>
+                    <i className="icon-clock" />
+                    {post.frontmatter.date}
+                  </Date>
                 </Details>
               </PostFooter>
             </Post>
